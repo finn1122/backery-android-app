bf6dc9022a5d67ef811923410790896d8c4f6e93
aspire <you@example.com>
2024-11-09 11:22:27 -0600

    mvvm, ya hace login pero no pasa nada, la peticion si llega a la api

--- app/src/main/java/com/example/mybakery/ui/register/RegisterScreen.kt
+++ app/src/main/java/com/example/mybakery/ui/register/RegisterScreen.kt
@@ -1,4 +1,4 @@
-package com.example.mybakery.ui.login
+package com.example.mybakery.ui.register
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
--- app/src/main/java/com/example/mybakery/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/mybakery/viewmodel/LoginViewModel.kt
@@ -4,9 +4,17 @@ import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mybakery.data.model.LoginCredentials
+import com.example.mybakery.data.network.RetrofitClient
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LoginViewModel : ViewModel() {
+
+    private val apiService = RetrofitClient.apiService
 
     private val _email = MutableLiveData<String>()
     val email : LiveData<String> = _email
@@ -20,6 +28,9 @@ class LoginViewModel : ViewModel() {
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading : LiveData<Boolean> = _isLoading
 
+    private val _loginResult = MutableLiveData<Result<String>>()
+    val loginResult: LiveData<Result<String>> = _loginResult
+
     fun onLoginChanged(email: String, password: String){
         _email.value = email
         _password.value = password
@@ -31,11 +42,22 @@ class LoginViewModel : ViewModel() {
 
     private fun isValidEmail(email: String): Boolean = Patterns.EMAIL_ADDRESS.matcher(email).matches()
 
-    suspend fun onLoginSelected(){
+    fun onLoginSelected(){
         _isLoading.value = true
-        delay(4000)
-        _isLoading.value = false
+        viewModelScope.launch {
+            val credentials = LoginCredentials(
+                email = _email.value ?: "",
+                password = _password.value ?: ""
+            )
+
+            try {
+                val response = withContext(Dispatchers.IO) { apiService.login(credentials) }
+                _loginResult.value = Result.success("Login successful: ${response.token}")
+            } catch (e: Exception) {
+                _loginResult.value = Result.failure(e)
+            } finally {
+                _isLoading.value = false
+            }
+        }
     }
-
-
 }
